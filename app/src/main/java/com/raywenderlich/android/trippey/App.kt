335ec407cb2f0b2c250002a0b7d08cf9df815a03
Niklas Rammerstorfer <rammerstorfer.n@gmail.com>
2026-03-26 09:04:23 +0100

    Layed ground work for storing data in `SharedPreferences`.

--- app/src/main/java/com/raywenderlich/android/trippey/App.kt
+++ app/src/main/java/com/raywenderlich/android/trippey/App.kt
@@ -40,14 +40,23 @@ import com.raywenderlich.android.trippey.repository.TrippeyRepositoryImpl
 
 class App : Application() {
 
-  companion object {
-    private lateinit var instance: App
+    companion object {
+        private const val KEY_PREFERENCES = "TrippeyPreferences"
 
-    val repository: TrippeyRepository by lazy { TrippeyRepositoryImpl() }
-  }
+        private lateinit var instance: App
 
-  override fun onCreate() {
-    super.onCreate()
-    instance = this
-  }
+        private val sharedPreferences by lazy {
+            instance.getSharedPreferences(
+                KEY_PREFERENCES,
+                MODE_PRIVATE
+            )
+        }
+
+        val repository: TrippeyRepository by lazy { TrippeyRepositoryImpl(sharedPreferences) }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
 }
